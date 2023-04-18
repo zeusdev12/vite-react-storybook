a12473eda9a8c2d3742e0d60185a65d37e6c3627
zeusdev12 <jasonzeus12@gmail.com>
2023-04-18 14:52:26 -0500

    add swap asset button and page component

---
+++ src/stories/BestRoute/AssetSwitchButton/AssetSwitchButton.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './AssetSwitchButton.scss';
+import SwapArrow from '../../assets/swap-arrow.svg';
+
+interface AssetSwitchButtonProps {
+  /**
+   * If `true`, the component is disabled.
+   */
+  disabled?: boolean;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const AssetSwitchButton = ({
+  disabled = false,
+  size = 'medium',
+  ...props
+}: AssetSwitchButtonProps) => {
+  const mode = disabled ? 'asset-switch-button--disabled' : '';
+  return (
+    <div
+      className={['asset-switch-button', `asset-switch-button--${size}`, mode].join(' ')}
+      {...props}
+    >
+      <img src={SwapArrow}/>
+    </div>
+  );
+};
