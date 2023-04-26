56c2a75a750fc53e866e3f83a08cb6e7de497471
zeusdev12 <jasonzeus12@gmail.com>
2023-04-26 16:30:58 -0500

    working for get best path via uniswap sdk

--- src/utils/uniswap.ts
+++ src/utils/uniswap.ts
@@ -3,11 +3,12 @@ import { CurrencyAmount, Percent, TradeType, Token } from "@uniswap/sdk-core";
 import _ from 'lodash';
 import { Protocol } from "@uniswap/router-sdk";
 import { ethers } from "ethers";
+import { Pool } from "@uniswap/v3-sdk";
 
-const RPC_URL = `https://eth-mainnet.alchemyapi.io/v2/${process.env.REACT_APP_ALCHEMY_API_KEY}`;
+const provider = ethers.getDefaultProvider(`https://eth-mainnet.alchemyapi.io/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}`);
+const signer = new ethers.Wallet(import.meta.env.VITE_PRIVATE_KEY as any, provider);
 
-const findUniswapBestRoute = async (
-  signer: ethers.Wallet, 
+export const findUniswapBestRoute = async (
   amountOut: string, 
   tokenIn: string,
   tokenInDecimal: number,
@@ -16,11 +17,10 @@ const findUniswapBestRoute = async (
   tokenOutDecimal: number,
   tokenOutSymbol: string
 ) => {
-  const provider = ethers.getDefaultProvider(RPC_URL);
   const recipient = await signer.getAddress();
   const router = new AlphaRouter({
     chainId: 1,
-    provider: provider,
+    provider: provider as any,
   })
   const options: SwapOptionsSwapRouter02 = {
     recipient: recipient,
@@ -56,23 +56,23 @@ const parseRoute = (
     CurrencyAmount.fromRawAmount(routeAmounts[0]!.amount.currency, 0)
   );
 
-  const bestRoute: V3Route | V2Route | MixedRoute = _.maxBy(routeAmounts, ({ amount }) => {
+  const bestRoute: V3Route | V2Route | MixedRoute | undefined = _.maxBy(routeAmounts, ({ amount }) => {
     const portion = amount.divide(total);
     const percent = new Percent(portion.numerator, portion.denominator);
     console.log('Percents: ', Number(percent.toFixed(2)));
     return Number(percent.toFixed(2));
-  }).route;
+  })?.route;
 
   const tokens =
-  bestRoute.protocol === Protocol.V3
+  bestRoute?.protocol === Protocol.V3
       ? bestRoute.tokenPath
       : // MixedRoute and V2Route have path
-      bestRoute.path;
+      bestRoute?.path;
   const tokenPath = _.map(tokens, (token) => `${token.symbol}`);
   const poolFees =
-  bestRoute.protocol === Protocol.V3 || bestRoute.protocol === Protocol.MIXED
-      ? bestRoute.pools.map((pool) => pool.fee)
-      : bestRoute.pairs.map((pair) => 3000);
+  bestRoute?.protocol === Protocol.V3 || bestRoute?.protocol === Protocol.MIXED
+      ? bestRoute.pools.map((pool) => (pool as Pool).fee)
+      : bestRoute?.pairs.map((pair) => 3000);
 
   console.log("SwapPath: ", tokenPath);
   console.log("PoolFees: ", poolFees);
