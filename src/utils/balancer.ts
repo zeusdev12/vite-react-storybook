592585680d8a8c3b6fd895e760e9a01fe72cd6f3
zeusdev12 <jasonzeus12@gmail.com>
2023-05-04 14:24:50 -0500

    fix uniswap sdk integration error by vite.config and integrated balancer sdk

---
+++ src/utils/balancer.ts
@@ -0,0 +1,41 @@
+import { BalancerSDK } from '@balancer-labs/sdk';
+
+const sdk = new BalancerSDK({
+  network: 1,
+  rpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}`
+});
+
+export const findBalancerBestRoute = async (
+  amountOut: string, 
+  tokenIn: string,
+  tokenInDecimal: number,
+  tokenInSymbol: string,
+  tokenOut: string,
+  tokenOutDecimal: number,
+  tokenOutSymbol: string
+) => {
+  const BALANCER_POOLIDS: { [key: string]: string } = {
+    '0xa13a9247ea42d743238089903570127dda72fe4400000000000000000000035d':
+      'BAL_BB_A_USD_LP',
+    '0x32296969ef14eb0c6d29669c550d4a0449130230000200000000000000000080':
+      'BAL_WSTETH_WETH_LP',
+    '0x1e19cf2d73a72ef1332c882f20534b6519be0276000200000000000000000112':
+      'BAL_RETH_WETH_LP',
+  };
+  
+  const pools = await sdk.pools.where((pool) =>
+    Object.keys(BALANCER_POOLIDS).includes(pool.id)
+  );
+  
+  const result = await Promise.all(
+    pools.map(async (pool) => {
+      const ret = await sdk.pools.apr(pool);
+      return {
+        ...ret,
+        symbol: BALANCER_POOLIDS[pool.id],
+      };
+    })
+  );
+
+  console.log(result);
+}
--- src/utils/uniswap.ts
+++ src/utils/uniswap.ts
@@ -1,12 +1,16 @@
-import { AlphaRouter, SwapOptionsSwapRouter02, SwapType, RouteWithValidQuote, V3Route, V2Route, MixedRoute } from "@uniswap/smart-order-router";
+import { AlphaRouter, RouteWithValidQuote, V3Route, V2Route, MixedRoute, SwapType, SwapOptionsSwapRouter02 } from "@uniswap/smart-order-router";
 import { CurrencyAmount, Percent, TradeType, Token } from "@uniswap/sdk-core";
 import _ from 'lodash';
 import { Protocol } from "@uniswap/router-sdk";
-import { ethers } from "ethers";
+import { JsonRpcProvider as Provider } from "@ethersproject/providers";
 import { Pool } from "@uniswap/v3-sdk";
 
-const provider = ethers.getDefaultProvider(`https://eth-mainnet.alchemyapi.io/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}`);
-const signer = new ethers.Wallet(import.meta.env.VITE_PRIVATE_KEY as any, provider);
+const provider = new Provider(`https://eth-mainnet.alchemyapi.io/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}`, 1);
+const user = '0x661fB502E24Deb30e927E39A38Bd2CC44D67339F';
+const router = new AlphaRouter({
+  chainId: 1,
+  provider: provider
+})
 
 export const findUniswapBestRoute = async (
   amountOut: string, 
@@ -17,13 +21,8 @@ export const findUniswapBestRoute = async (
   tokenOutDecimal: number,
   tokenOutSymbol: string
 ) => {
-  const recipient = await signer.getAddress();
-  const router = new AlphaRouter({
-    chainId: 1,
-    provider: provider as any,
-  })
   const options: SwapOptionsSwapRouter02 = {
-    recipient: recipient,
+    recipient: user,
     slippageTolerance: new Percent(50, 10_000),
     deadline: Math.floor(Date.now() / 1000 + 1800),
     type: SwapType.SWAP_ROUTER_02,
@@ -33,7 +32,6 @@ export const findUniswapBestRoute = async (
     new Token(1, tokenOut, tokenOutDecimal, tokenOutSymbol),
     amountOut
   );
-  console.log("DebtAmount: ", tokenAmountOut.toFixed(0));
 
   const route = await router.route(
     tokenAmountOut,
@@ -59,7 +57,7 @@ const parseRoute = (
   const bestRoute: V3Route | V2Route | MixedRoute | undefined = _.maxBy(routeAmounts, ({ amount }) => {
     const portion = amount.divide(total);
     const percent = new Percent(portion.numerator, portion.denominator);
-    console.log('Percents: ', Number(percent.toFixed(2)));
+    console.log('Best Route Percents: ', Number(percent.toFixed(2)));
     return Number(percent.toFixed(2));
   })?.route;
 
@@ -73,9 +71,6 @@ const parseRoute = (
   bestRoute?.protocol === Protocol.V3 || bestRoute?.protocol === Protocol.MIXED
       ? bestRoute.pools.map((pool) => (pool as Pool).fee)
       : bestRoute?.pairs.map((pair) => 3000);
-
-  console.log("SwapPath: ", tokenPath);
-  console.log("PoolFees: ", poolFees);
 
   return { tokenPath, poolFees }
 };
