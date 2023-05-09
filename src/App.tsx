c54f62aff68ea04d0a14a5a0829beb2f352af220
zeusdev12 <jasonzeus12@gmail.com>
2023-05-09 14:40:05 -0500

    working with balancer sdk to find best swap route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { findUniswapBestRoute } from './utils/uniswap';
-import { findBalancerBestRoute } from './utils/balancer';
+import { balancerInit, findBalancerBestRoute } from './utils/balancer';
 import { curveInit, findCurveBestRoute } from './utils/curve';
 
 function App() {
@@ -19,15 +19,13 @@ function App() {
     'FRAX'
   ).then(result => console.log(result));
 
-  findBalancerBestRoute(
-    '1000',
-    '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-    6,
-    'USDC',
-    '0x853d955aCEf822Db058eb8505911ED77F175b99e',
-    18,
-    'FRAX'
-  ).then(result => console.log(result));
+  balancerInit().then(() => {
+    findBalancerBestRoute(
+      '1000000000',
+      '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+      '0x853d955aCEf822Db058eb8505911ED77F175b99e',
+    ).then(result => console.log(result));
+  });
 
   curveInit().then(() => {
     findCurveBestRoute(
@@ -35,7 +33,7 @@ function App() {
       '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
       '0x853d955aCEf822Db058eb8505911ED77F175b99e',
     ).then(result => console.log(result));
-  })
+  });
 
   return (
     <div className="App">
--- src/utils/balancer.ts
+++ src/utils/balancer.ts
@@ -1,41 +1,27 @@
 import { BalancerSDK } from '@balancer-labs/sdk';
+import { BigNumber } from 'ethers';
 
 const sdk = new BalancerSDK({
   network: 1,
   rpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${import.meta.env.VITE_ALCHEMY_API_KEY}`
 });
 
+export const balancerInit = async () => {
+  await sdk.swaps.fetchPools();
+}
+
 export const findBalancerBestRoute = async (
   amountIn: string, 
   tokenIn: string,
-  tokenInDecimal: number,
-  tokenInSymbol: string,
-  tokenOut: string,
-  tokenOutDecimal: number,
-  tokenOutSymbol: string
+  tokenOut: string
 ) => {
-  const BALANCER_POOLIDS: { [key: string]: string } = {
-    '0xa13a9247ea42d743238089903570127dda72fe4400000000000000000000035d':
-      'BAL_BB_A_USD_LP',
-    '0x32296969ef14eb0c6d29669c550d4a0449130230000200000000000000000080':
-      'BAL_WSTETH_WETH_LP',
-    '0x1e19cf2d73a72ef1332c882f20534b6519be0276000200000000000000000112':
-      'BAL_RETH_WETH_LP',
-  };
-  
-  const pools = await sdk.pools.where((pool) =>
-    Object.keys(BALANCER_POOLIDS).includes(pool.id)
-  );
-  
-  const result = await Promise.all(
-    pools.map(async (pool) => {
-      const ret = await sdk.pools.apr(pool);
-      return {
-        ...ret,
-        symbol: BALANCER_POOLIDS[pool.id],
-      };
-    })
-  );
+  const route = await sdk.swaps.findRouteGivenIn({
+    tokenIn,
+    tokenOut,
+    amount: BigNumber.from(amountIn),
+    gasPrice: BigNumber.from(0),
+    maxPools: 1
+  });
 
-  console.log(result);
+  return route;
 }
